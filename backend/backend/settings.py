347f0a4dd8417994255c9e19f9e93155886a9784
tvrda34 <tvrtko.ivasic3@gmail.com>
2022-05-14 22:04:03 +0200

    django conf changes

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv, find_dotenv
+import os
+
+load_dotenv(find_dotenv())
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-c*$n^6qsi_ra44g_#$7zy6h^+(_v1gp2sov9p4j1btdw(#6ow$'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -123,11 +127,23 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+# DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+# }
+
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+   'default': {
+       'ENGINE': 'django.db.backends.postgresql',
+       'NAME': os.environ['DB_NAME'],
+       'USER':  os.environ['DB_USER'],
+       'PASSWORD': os.environ['DB_PASSWORD'],
+       'HOST': os.environ['DB_HOST'],
+       'PORT': '5432'
+   }
 }
 
 
@@ -191,5 +207,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = '587'
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'tfiskaltest@gmail.com'
-EMAIL_HOST_PASSWORD = 'Fte.st00AL'
+EMAIL_HOST_USER = os.environ['MAIL_USER']
+EMAIL_HOST_PASSWORD = os.environ['MAIL_PASSWORD']
